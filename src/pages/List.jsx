6cf8b8106ee399ae5b9795b2367e98d710edaa67
Cem Tanrikut <cemtanrikut@gmail.com>
2025-03-19 18:12:51 +0100

    bug fix

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -619,7 +619,7 @@ export default function List() {
             return `https://api-osius.up.railway.app/${fileURL}`;
         }
         // TODO: Silinecek
-        fileURL.replace("http://localhost:8080", "");
+        fileURL = fileURL.replace("http://localhost:8080/", "");
         console.log("GELEN URL : ", fileURL)
         return `https://api-osius.up.railway.app/${fileURL}`;
         //return fileURL;
